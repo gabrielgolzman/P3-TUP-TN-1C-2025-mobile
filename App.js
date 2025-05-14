528ae5e89a2853b5cc77a6e7e22233e64aa0c5c0
Gabriel Golzman <ggolzman@primerosystems.com>
2025-05-14 19:48:16 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (enteredText) => {
+    setText(enteredText);
+  }
+
+  const handlePressAdd = () => {
+    Alert.alert('Agregar lectura', `Lectura ${text} agregada`, [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.rootScreen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Ingrese la lectura...'
+          onChangeText={handleTextChange}
+          value={text}
+        />
+        <View style={styles.buttonInput}>
+          <Button title='Agregar lectura' onPress={handlePressAdd} />
+        </View>
+      </View>
+      <View>
+        <Text>Lista de lecturas...</Text>
+      </View>
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
   },
+  inputContainer: {
+    justifyContent: 'center'
+  },
+  textInput: {
+    width: "100%",
+    marginRight: 8,
+    marginVertical: 15,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "#CCCCCC"
+  },
+  buttonInput: {
+    width: "50%",
+    alignSelf: "flex-end"
+  }
+
+
 });
