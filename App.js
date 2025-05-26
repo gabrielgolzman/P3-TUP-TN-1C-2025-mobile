ebb042940f07b9a4eec2f5e20d7ba4745448ec0d
Gabriel Golzman <ggolzman@primerosystems.com>
2025-05-26 17:25:13 -0300

    Improved composition

--- App.js
+++ App.js
@@ -1,41 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import ReadingList from './components/readingList/ReadingList';
 
 const App = () => {
-  const [text, setText] = useState('');
-
-  const handleTextChange = (enteredText) => {
-    setText(enteredText);
-  }
-
-  const handlePressAdd = () => {
-    Alert.alert('Agregar lectura', `Lectura ${text} agregada`, [
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      { text: 'OK', onPress: () => console.log('OK Pressed') },
-    ])
-  }
-
   return (
     <View style={styles.rootScreen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Ingrese la lectura...'
-          onChangeText={handleTextChange}
-          value={text}
-        />
-        <View style={styles.buttonInput}>
-          <Button title='Agregar lectura' onPress={handlePressAdd} />
-        </View>
-      </View>
-      <View>
-        <Text>Lista de lecturas...</Text>
-      </View>
+      <ReadingList />
     </View>
   );
 }
@@ -47,21 +18,5 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 50,
   },
-  inputContainer: {
-    justifyContent: 'center'
-  },
-  textInput: {
-    width: "100%",
-    marginRight: 8,
-    marginVertical: 15,
-    padding: 8,
-    borderWidth: 1,
-    borderColor: "#CCCCCC"
-  },
-  buttonInput: {
-    width: "50%",
-    alignSelf: "flex-end"
-  }
-
 
 });
---
+++ components/readingList/ReadingList.jsx
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const ReadingList = () => {
+    const [text, setText] = useState('');
+
+    const handleTextChange = (enteredText) => {
+        setText(enteredText);
+    }
+
+    const handlePressAdd = () => {
+        Alert.alert('Agregar lectura', `Lectura ${text} agregada`, [
+            {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ])
+    }
+    return (
+        <>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder='Ingrese la lectura...'
+                    onChangeText={handleTextChange}
+                    value={text}
+                />
+                <View style={styles.buttonInput}>
+                    <Button title='Agregar lectura' onPress={handlePressAdd} />
+                </View>
+            </View>
+            <View>
+                <Text>Lista de lecturas...</Text>
+            </View>
+        </>
+    )
+}
+
+export default ReadingList
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: 'center'
+    },
+    textInput: {
+        width: "100%",
+        marginRight: 8,
+        marginVertical: 15,
+        padding: 8,
+        borderWidth: 1,
+        borderColor: "#CCCCCC"
+    },
+    buttonInput: {
+        width: "50%",
+        alignSelf: "flex-end"
+    }
+})
